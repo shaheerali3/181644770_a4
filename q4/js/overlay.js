ef4fd14e12f9391ff9016427f16c776f7f40c979
Shaheerali <alix4770@mylaurier.ca>
2019-12-11 00:12:10 -0500

    first commit

---
+++ q4/js/overlay.js
@@ -0,0 +1,7 @@
+function on(imgURL) {
+      document.getElementById("myImage").src = imgURL;
+      document.getElementById("overlay").style.display = "block";
+    }
+    function off() {
+      document.getElementById("overlay").style.display = "none";
+    }
